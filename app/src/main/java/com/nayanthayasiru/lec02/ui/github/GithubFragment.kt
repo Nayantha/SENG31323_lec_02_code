ef54f0941572165c10dc3a1f1071390b638c417c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-13 15:40:49 +0530

    changed to data binging technique.

--- app/src/main/java/com/nayanthayasiru/lec02/ui/github/GithubFragment.kt
+++ app/src/main/java/com/nayanthayasiru/lec02/ui/github/GithubFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.nayanthayasiru.lec02.R
+import com.nayanthayasiru.lec02.databinding.FragmentGithubBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -17,18 +20,22 @@ class GithubFragment : Fragment() {
     }
 
     private lateinit var viewModel: GithubViewModel
-
+    private lateinit var binding: FragmentGithubBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_github, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    ): View {
         viewModel = ViewModelProvider(this).get(GithubViewModel::class.java)
-        // TODO: Use the ViewModel
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_github, container, false)
+        val recycleView = binding.usersRecyclerview
+        val adapter = UserListAdapter(binding.root.context, this, viewModel)
+        recycleView.adapter = adapter
+        recycleView.layoutManager = LinearLayoutManager(binding.root.context)
+        viewModel.users.observe(viewLifecycleOwner) {
+            adapter.setUsers(it)
+        }
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+        return binding.root
     }
-
 }
