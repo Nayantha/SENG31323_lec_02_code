de48ca7db69db66e465d122d297148f328524cf2
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-13 12:01:09 +0530

    add data retrieval method and a data clean up class.

--- app/src/main/java/com/nayanthayasiru/lec02/ui/github/GithubService.kt
+++ app/src/main/java/com/nayanthayasiru/lec02/ui/github/GithubService.kt
@@ -1,4 +1,13 @@
 package com.nayanthayasiru.lec02.ui.github
 
-class GithubService {
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface GithubService {
+    @GET("users")
+    suspend fun users(): Response<List<GithubUser>>
 }
+
+data class GithubUser(var login: String, var avatar_url: String)
