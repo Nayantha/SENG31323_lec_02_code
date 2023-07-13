0866800a8d6a5ffb09d1e89357711bb60d07098f
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-13 15:41:33 +0530

    add variables to store retrieved user list.

--- app/src/main/java/com/nayanthayasiru/lec02/ui/github/GithubViewModel.kt
+++ app/src/main/java/com/nayanthayasiru/lec02/ui/github/GithubViewModel.kt
@@ -1,5 +1,6 @@
 package com.nayanthayasiru.lec02.ui.github
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -9,10 +10,13 @@ import javax.inject.Inject
 @HiltViewModel
 class GithubViewModel @Inject constructor(
     private var githubService: GithubService
-) : ViewModel() {
+) : ViewModel(){
+    private val _users = MutableLiveData<List<GithubUser>>()
+    val users = _users
     init {
         viewModelScope.launch{
             val response = githubService.users()
+            _users.value = response.body()
         }
     }
 }
