a88431c68118b7eae8e85434b6360cd7ec6cbff8
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-13 10:46:57 +0530

    Initial commit

---
+++ app/src/main/java/com/nayanthayasiru/lec02/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.nayanthayasiru.lec02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nayanthayasiru.lec02.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
