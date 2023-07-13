6b1bed8f3ad33b68930f54825f7a40556fe7e09f
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-13 12:27:46 +0530

    add fun to call the Model and retrieve data.

--- app/src/main/java/com/nayanthayasiru/lec02/ui/github/GithubViewModel.kt
+++ app/src/main/java/com/nayanthayasiru/lec02/ui/github/GithubViewModel.kt
@@ -1,7 +1,18 @@
 package com.nayanthayasiru.lec02.ui.github
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class GithubViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class GithubViewModel @Inject constructor(
+    private var githubService: GithubService
+) : ViewModel() {
+    init {
+        viewModelScope.launch{
+            val response = githubService.users()
+        }
+    }
 }
