3b3bda67d65fb460c5e62c9396fed6f384871436
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-13 12:01:52 +0530

    optimize imports.

--- app/src/main/java/com/nayanthayasiru/lec02/ui/github/GithubService.kt
+++ app/src/main/java/com/nayanthayasiru/lec02/ui/github/GithubService.kt
@@ -1,9 +1,7 @@
 package com.nayanthayasiru.lec02.ui.github
 
-import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Path
 
 interface GithubService {
     @GET("users")
