811faa2cb9e4a1266f1a09592da5298887dabba4
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-13 11:46:59 +0530

    add function to build retrofit instance.

--- app/src/main/java/com/nayanthayasiru/lec02/di/AppModule.kt
+++ app/src/main/java/com/nayanthayasiru/lec02/di/AppModule.kt
@@ -1,10 +1,16 @@
 package com.nayanthayasiru.lec02.di
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
+import retrofit2.Retrofit
 
 @Module
 @InstallIn(ActivityComponent::class)
 class AppModule {
+    @Provides
+    fun provideRetrofit(): Retrofit = Retrofit.Builder()
+        .baseUrl("https://api.github.com")
+        .build()
 }
