c6703036a07ee76a6a7c061cb562c5893a9d6b0d
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-13 11:36:32 +0530

    add hilt modules.

--- app/src/main/java/com/nayanthayasiru/lec02/di/AppModule.kt
+++ app/src/main/java/com/nayanthayasiru/lec02/di/AppModule.kt
@@ -1,4 +1,10 @@
 package com.nayanthayasiru.lec02.di
 
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+
+@Module
+@InstallIn(ActivityComponent::class)
 class AppModule {
 }
