27dff1cf316b9173181a7ae553ee7290cbac79f6
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-13 12:12:37 +0530

    add github fragment with view model.

---
+++ app/src/main/java/com/nayanthayasiru/lec02/ui/github/GithubFragment.kt
@@ -0,0 +1,32 @@
+package com.nayanthayasiru.lec02.ui.github
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.nayanthayasiru.lec02.R
+
+class GithubFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = GithubFragment()
+    }
+
+    private lateinit var viewModel: GithubViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_github, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(GithubViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/nayanthayasiru/lec02/ui/github/GithubViewModel.kt
@@ -0,0 +1,7 @@
+package com.nayanthayasiru.lec02.ui.github
+
+import androidx.lifecycle.ViewModel
+
+class GithubViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
