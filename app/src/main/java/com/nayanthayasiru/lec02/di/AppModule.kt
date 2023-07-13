4c8b088a8e5af2f106e6463e9a00826106229829
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-13 12:29:55 +0530

    add Singleton attributes to class.

--- app/src/main/java/com/nayanthayasiru/lec02/di/AppModule.kt
+++ app/src/main/java/com/nayanthayasiru/lec02/di/AppModule.kt
@@ -4,18 +4,21 @@ import com.nayanthayasiru.lec02.ui.github.GithubService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
-@InstallIn(ActivityComponent::class)
+@InstallIn(SingletonComponent::class)
 class AppModule {
+    @Singleton
     @Provides
     fun provideRetrofit(): Retrofit = Retrofit.Builder()
         .baseUrl("https://api.github.com")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
+    @Singleton
     @Provides
     fun provideGithubService(retrofit: Retrofit):GithubService = retrofit.create(GithubService::class.java)
 }
