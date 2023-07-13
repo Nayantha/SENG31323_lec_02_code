3a62c72a58253a9f7d29545a18a58ec90fa55f7d
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-13 11:23:47 +0530

    inject hilt dependencies to class.

--- app/src/main/java/com/nayanthayasiru/lec02/MainActivity.kt
+++ app/src/main/java/com/nayanthayasiru/lec02/MainActivity.kt
@@ -3,7 +3,9 @@ package com.nayanthayasiru.lec02
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.nayanthayasiru.lec02.ui.main.MainFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
